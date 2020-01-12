88f9dd11b62c62fe5db02014bd1d5378b28a4b73
nim-z <nimits4900@gmail.com>
2020-01-12 21:20:38 +0530

    Initial commit

---
+++ app/src/main/java/com/test/notes/ListAdapter.java
@@ -0,0 +1,37 @@
+package com.test.notes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> {
+
+    ArrayList<String> mDataset;
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView textview;
+        public MyViewHolder(View v){
+            super(v);
+            textview=(TextView) v.findViewById(R.id.filename);
+        }
+    }
+
+    public ListAdapter(ArrayList<String> myDataset) {
+        mDataset=myDataset;
+    }
+    public ListAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,int viewType){
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_row,parent,false);
+        return new MyViewHolder(v);
+    }
+    public void onBindViewHolder(MyViewHolder holder,int position) {
+        holder.textview.setText(mDataset.get(position));
+    }
+    public int getItemCount(){
+        return mDataset.size();
+    }
+
+}
---
+++ app/src/main/java/com/test/notes/MainActivity.java
@@ -0,0 +1,72 @@
+package com.test.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    ListAdapter mAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button makenewnote;
+        recyclerView=findViewById(R.id.recyclerView);
+        //recyclerView.setHasFixedSize(true);
+        File[] files=getFilesDir().listFiles();
+        ArrayList<String> mdataset=new ArrayList<String>(10);
+        mdataset.add("first");
+        for(int i=0;i<files.length;i++)
+        {
+            Log.d("Exception","File name : "+files[i].getName());
+            mdataset.add(files[i].getName());
+        }
+        mAdapter=new ListAdapter(mdataset);
+        recyclerView.setAdapter(mAdapter);
+        layoutManager=new LinearLayoutManager(this);
+
+        layoutManager.canScrollVertically();
+        recyclerView.setLayoutManager(layoutManager);
+
+        makenewnote=(Button)findViewById(R.id.createnew);
+        makenewnote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this,newnote.class),1);
+            }
+        });
+    }
+
+     @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==1 && resultCode== Activity.RESULT_OK){
+            String newnotetitle=data.getStringExtra("newnote");
+
+            boolean isPresent=false;
+            File[] files=getFilesDir().listFiles();
+            for(int i=0;i<files.length && !isPresent;i++)
+            {
+                isPresent=newnotetitle.equals(files[i]);
+            }
+            if(!isPresent) {
+                mAdapter.mDataset.add(newnotetitle);
+                recyclerView.setAdapter(mAdapter);
+            }
+        } }
+}
